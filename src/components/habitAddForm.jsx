676a7511bf116711042492c2b6fa52da006693e3
KJH <dev.jihyun@gmail.com>
2021-07-10 11:15:49 +0900

    Habit-Tracker

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+    onSubmit = event => {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form className="add-form" onSubmit={this.onSubmit}>
+                <input type="text" className="add-input" placeholder="Habit" />
+                <button className="add-button">add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
